bef5a8a83283ec7a2d78789661b8568602b20fa4
StevenKligman <kligmasn@mail.uc.edu>
2018-02-22 20:45:20 -0500

    new stuff

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-var scene, camera, cube, renderer;
-function init(){
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.z = 15;
-
-    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-}
-function animate() {
-    requestAnimationFrame( animate );
-    
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-	renderer.render( scene, camera );
-}
+var camera, scene, renderer, geometry, material, mesh;
 
 init();
 animate();
+
+function init() {
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 500;
+    scene.add(camera);
+
+    geometry = new THREE.CubeGeometry(200, 200, 200);
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    render();
+
+}
+
+function render() {
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+
+}
