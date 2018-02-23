9c537f7ebb40c5d4bb152bcc70790261ef717ce0
StevenKligman <kligmasn@mail.uc.edu>
2018-02-22 20:20:55 -0500

    new stuff

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-scene.add(camera);
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
