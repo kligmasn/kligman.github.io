caa507f83c1d05b175d3af82ae61e30ca9945ea4
StevenKligman <kligmasn@mail.uc.edu>
2018-02-22 20:14:41 -0500

    new stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+scene.add(camera);
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -16,7 +16,7 @@ function animate() {
     
     cube.rotation.x += 0.1;
     cube.rotation.y += 0.1;
-    
+
 	renderer.render( scene, camera );
 }
 animate();
